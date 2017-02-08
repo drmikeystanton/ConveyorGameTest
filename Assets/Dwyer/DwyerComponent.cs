b43a4c9d7fa6fb39de670d29180edc718ec866de
dumaike <mdwyer84@gmail.com>
2017-02-07 21:35:40 -0500

    Stuff to test random outside Monobehaviours and also list copying

---
+++ Assets/Dwyer/DwyerComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DwyerComponent : MonoBehaviour
+{	
+	// Use this for initialization
+	void Start ()
+	{
+		DwyerNotComponent dnc1 = new DwyerNotComponent();
+		DwyerNotComponent dnc2 = new DwyerNotComponent();
+
+		Debug.Log("We made two random lists " + dnc1 + " and " + dnc2);
+
+		dnc2.CopyList(dnc1.myList);
+
+		Debug.Log("Copying 1 to 2 and now they're " + dnc1 + " and " + dnc2);
+
+		dnc1.GenerateList();
+
+		Debug.Log("Regenerating 1 to confirm it copied, and isn't a reference and now they're " + dnc1 + " and " + dnc2);
+	}	
+}
---
+++ Assets/Dwyer/DwyerNotComponent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DwyerNotComponent
+{
+	public List<int> myList = new List<int>();
+
+	public DwyerNotComponent()
+	{
+		GenerateList();
+	}
+
+	public void CopyList(List<int> newList)
+	{
+		myList = new List<int>(newList);
+	}
+
+	public void GenerateList()
+	{
+		myList = new List<int>();
+
+		int listSize = (int)(Random.Range(3, 6));
+		for (int i = 0; i < listSize; ++i)
+		{
+			myList.Add((int)Random.Range(10, 500));
+		}
+	}
+
+	public override string ToString()
+	{
+		string printedString = "{";
+		for (int i = 0; i < myList.Count; ++i)
+		{
+			printedString += myList[i] + ", ";
+		}
+
+		//Remove the trailing comma if the list has any items in it
+		if (myList.Count > 0)
+		{
+			printedString = printedString.Substring(0, printedString.Length - 2);
+		}
+
+		printedString += "}";
+
+		return printedString;
+	}
+}
